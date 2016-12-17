40b914cf594695e6b10a5956cf0cb02efb88059e
Alex Markley <alex@mbe.tv>
2016-12-16 21:45:36 -0500

    don't leak memory, even in testing!

--- test/ProductTest.c
+++ test/ProductTest.c
@@ -14,43 +14,51 @@ void test_ProductDestroyShouldReturnNull(void) {
 void test_ProductGetStockShouldStartUninitialized(void) {
 	Product *prod = ProductCreate();
 	TEST_ASSERT_EQUAL_INT8(PRODUCT_STOCK_UNINITIALIZED, ProductGetStock(prod));
+	ProductDestroy(prod);
 }
 
 void test_ProductSetStockShouldReturnTrue(void) {
 	Product *prod = ProductCreate();
 	TEST_ASSERT_TRUE(ProductSetStock(prod, 35));
+	ProductDestroy(prod);
 }
 
 void test_ProductSetStockShouldPersist(void) {
 	Product *prod = ProductCreate();
 	TEST_ASSERT_TRUE(ProductSetStock(prod, 48));
 	TEST_ASSERT_EQUAL_INT8(48, ProductGetStock(prod));
+	ProductDestroy(prod);
 }
 
 void test_ProductSetStockInvalidValueShouldReturnFalse(void) {
 	Product *prod = ProductCreate();
 	TEST_ASSERT_FALSE(ProductSetStock(prod, 128));
+	ProductDestroy(prod);
 }
 
 void test_ProductGetValueShouldStartUninitialized(void) {
 	Product *prod = ProductCreate();
 	TEST_ASSERT_EQUAL_INT8(PRODUCT_VALUE_UNINITIALIZED, ProductGetValue(prod));
+	ProductDestroy(prod);
 }
 
 void test_ProductSetValueShouldReturnTrue(void) {
 	Product *prod = ProductCreate();
 	TEST_ASSERT_TRUE(ProductSetValue(prod, 100));
+	ProductDestroy(prod);
 }
 
 void test_ProductSetValueShouldPersist(void) {
 	Product *prod = ProductCreate();
 	TEST_ASSERT_TRUE(ProductSetValue(prod, 65));
 	TEST_ASSERT_EQUAL_INT8(65, ProductGetValue(prod));
+	ProductDestroy(prod);
 }
 
 void test_ProductSetValueInvalidValueShouldReturnFalse(void) {
 	Product *prod = ProductCreate();
 	TEST_ASSERT_FALSE(ProductSetValue(prod, 128));
+	ProductDestroy(prod);
 }
 
 int main(void) {
